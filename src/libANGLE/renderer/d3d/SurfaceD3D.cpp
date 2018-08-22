7ac6b6c86f47664b0f29b4bfcdcfc8bff807b4d9
Oliver Wolff <oliver.wolff@qt.io>
2018-08-22 12:58:13 +0200

    ANGLE: Add support for querying platform device
    The EGL_EXT_device_base extension allows for querying the platform
    device of the graphics hardware via eglQueryDisplayAttribEXT().
    As that extension is not supported by ANGLE, this patch adds similar
    functionality to the existing eglQuerySurfacePointerANGLE API. When
    EGL_DEVICE_EXT is passed as the queried attribute, the underlying
    D3D/DXGI device pointer is passed back to the caller via the value
    argument.
    
    The D3D device is needed for video support in QtMultimedia as well as
    the IDXGIDevice3::Trim() calls required by the Windows Store.
    
    Change-Id: Ib3dfd3edc47dbcc02b07f71980ba785508f2ee57

--- src/libANGLE/renderer/d3d/SurfaceD3D.cpp
+++ src/libANGLE/renderer/d3d/SurfaceD3D.cpp
@@ -351,6 +351,10 @@ egl::Error SurfaceD3D::querySurfacePointerANGLE(EGLint attribute, void **value)
     {
         *value = mSwapChain->getKeyedMutex();
     }
+    else if (attribute == EGL_DEVICE_EXT)
+    {
+        *value = mSwapChain->getDevice();
+    }
     else UNREACHABLE();
 
     return egl::NoError();
