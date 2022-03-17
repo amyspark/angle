ba524a9c44eebfb86ddd8e028dbb99581455ba9d
Alvin Wong <alvinhochun@gmail.com>
2022-03-17 16:12:08 +0800

    Reduce flickering when resizing window
    - Do not swap buffers when resizing swapchain, because it may cause
      visual artifacts when resizing the window.
    - Do not stretch buffer with DXGI 1.2 swap chain. This stops the window
      content from being stretched when resizing the window.
    
    See:
    
    https://invent.kde.org/graphics/krita/-/blob/master/3rdparty/ext_googleangle/02-patches_krita/0016-Reduce-flickering-when-resizing-window.patch
    
    Signed-off-by: L. E. Segovia <amy@amyspark.me>
    Change-Id: I249b0b64d5cc59da1ed99ca3fec89275e177a259

--- src/libANGLE/renderer/d3d/d3d11/win32/NativeWindow11Win32.cpp
+++ src/libANGLE/renderer/d3d/d3d11/win32/NativeWindow11Win32.cpp
@@ -160,7 +160,7 @@ HRESULT NativeWindow11Win32::createSwapChain(ID3D11Device *device,
         swapChainDesc.BufferUsage =
             DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT | DXGI_USAGE_BACK_BUFFER;
         swapChainDesc.BufferCount   = 1;
-        swapChainDesc.Scaling       = DXGI_SCALING_STRETCH;
+        swapChainDesc.Scaling       = DXGI_SCALING_NONE;
         swapChainDesc.SwapEffect    = DXGI_SWAP_EFFECT_SEQUENTIAL;
         swapChainDesc.AlphaMode     = DXGI_ALPHA_MODE_UNSPECIFIED;
         swapChainDesc.Flags         = 0;
